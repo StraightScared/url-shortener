c4c494ad70b117163b625ca386721910fb1004a7
Brandon Sims <bsims272004@gmail.com>
2025-02-11 01:45:43 -0500

    Updated3

--- hello/models.py
+++ hello/models.py
@@ -14,7 +14,7 @@ def generate_short_link():
 
 class ShortenedURL(models.Model):
     short_link = models.CharField(max_length=20, unique=True, default=generate_short_link())
-    original_url = models.URLField
+    original_url = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.short_link} -> {self.original_url}"
