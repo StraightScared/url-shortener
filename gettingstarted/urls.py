8127cc77bdb892b8299350421562c336da90a64c
Brandon Sims <bsims272004@gmail.com>
2025-02-11 13:07:19 -0500

    Lets see?

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 # from django.contrib import admin
 from django.urls import path
-from hello.views import index, redirect_url, db
+from hello.views import index, redirect_url
 
 urlpatterns = [
     path("", index, name="index"),
